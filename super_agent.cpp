be47ab2fd4d8cc7431af6455eec76168b00ab3b6
AjayBadhe <ajaybadhe2024@gmail.com>
2025-12-09 00:03:20 +0530

    Super_Agent

---
+++ super_agent.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<string>a(3);
+    for(int i=0;i<3;i++){
+      cin>>a[i];
+    }
+    if(a[0][0]==a[2][2] &&
+       a[0][1]==a[2][1] &&
+       a[0][2]==a[2][0] &&
+       a[1][0]==a[1][2]){
+         cout<<"YES"<<endl;
+       }else{
+         cout<<"NO"<<endl;
+       }
+
+return 0;
+}
