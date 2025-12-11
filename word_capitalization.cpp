f8b3a64fbba40879f308335b252b25ce1d11f9a5
AjayBadhe <ajaybadhe2024@gmail.com>
2025-12-11 09:04:51 +0530

    Word_Capitalization

---
+++ word_capitalization.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     string s;
+     cin>>s;
+     //convert first letter into uppercase
+     s[0]=toupper(s[0]);
+     cout<<s<<endl;
+return 0;
+}
