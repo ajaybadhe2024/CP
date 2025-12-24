14ce1b13691a126986b5f125682fa8b344dc851b
AjayBadhe <ajaybadhe2024@gmail.com>
2025-12-24 18:32:16 +0530

    Blackslex_Password

---
+++ blackslex_password.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int t;
+   cin>>t;
+  for(int i=0;i<t;i++){
+    int k,x;
+    cin>>k>>x;
+    int n = k * x;
+    int ans=k*x+1;
+    cout<<ans<<endl;
+
+   }
+return 0;
+}
