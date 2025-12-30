0a7f8e2b0e8a897402a93000e1ca5e1340f35670
AjayBadhe <ajaybadhe2024@gmail.com>
2025-12-30 17:04:04 +0530

    Year_String

---
+++ year_string.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+     int n;
+     cin>>n;
+     string s;
+     cin>>s;
+     if(s.find("2025")!=string::npos&&s.find("2026")!=string::npos){
+        cout<<0<<endl;
+    }else if(s.find("2025")!=string::npos){
+        cout<<1<<endl;
+    }else{
+        cout<<0<<endl;
+    }
+    }
+return 0;
+}
