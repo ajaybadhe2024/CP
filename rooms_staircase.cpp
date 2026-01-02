6d7f8f5460754250f25c602fc0fd231924e6accc
AjayBadhe <ajaybadhe2024@gmail.com>
2026-01-02 13:22:43 +0530

    Rooms_Staircase

---
+++ rooms_staircase.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     int t;
+     cin>>t;
+     while(t--){
+       int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        int ans=n;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            cnt++;
+            if(s[i]=='1'){
+                ans=max(ans,(i+1)*2);
+                cnt++;
+            }
+        }
+        ans=max(ans,cnt);
+        reverse(s.begin(),s.end());
+        cnt=0;
+        for(int i=0;i<n;i++){
+            cnt++;
+            if(s[i]=='1'){
+                ans=max(ans,(i+1)*2);
+                cnt++;
+            }
+        }
+        ans=max(ans,cnt);
+
+        cout<<ans<<endl;
+     }
+return 0;
+}
