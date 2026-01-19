0f150e86ec11c5b01a380f4161de8588a5fb4f60
AjayBadhe <ajaybadhe2024@gmail.com>
2026-01-20 00:04:36 +0530

    Prime_Subtration

---
+++ prime_subtraction.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     long long t;
+     cin>>t;
+     while(t--){
+        long long x,y;
+        cin>>x>>y;
+        long long diff=x-y;
+        if(diff==1){
+            cout<<"NO"<<endl;
+        }else{
+            cout<<"YES"<<endl;
+        }
+     }
+return 0;
+}
