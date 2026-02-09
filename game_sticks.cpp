05ec47a3d0a08076e6b0ec6d8f8ba8deb7f84d69
AjayBadhe <ajaybadhe2024@gmail.com>
2026-02-09 19:25:11 +0530

    Game_Stick

---
+++ game_sticks.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     long long n,m;
+     cin>>n>>m;
+     if(min(n,m)%2==1){
+        cout<<"Akshat"<<endl;
+     }
+     else{
+        cout<<"Malvika"<<endl;
+     }
+return 0;
+}
