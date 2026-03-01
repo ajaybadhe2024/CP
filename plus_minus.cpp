b4a96ef1173d775da13cda12887a8702ddc60869
AjayBadhe <ajaybadhe2024@gmail.com>
2026-03-01 23:31:04 +0530

    Plus_Minus

---
+++ plus_minus.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int t;
+   cin>>t;
+   while(t--){
+    int a,b,c;
+    cin>>a>>b>>c;
+    int sum=a+b;
+    int sub=a-b;
+    if(sum==c){
+      cout<<"+"<<endl;
+    }else{
+      cout<<"-"<<endl;
+    }
+   }
+return 0;
+}
