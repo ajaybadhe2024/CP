5fcf65ba800cb3cae3a7bfe832d9a35b9af9f002
AjayBadhe <ajaybadhe2024@gmail.com>
2026-03-06 12:32:08 +0530

    Candies

---
+++ candies.cpp
@@ -0,0 +1,19 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     int t;
+     cin>>t;
+     while(t--){
+        long long n;
+        cin>>n;
+        for(int k=2;k<30;k++){
+            long long sum= (1<<k)-1;
+            if(n%sum==0){
+                cout<<n/sum<<endl;
+                break;
+            }
+        }
+     }
+return 0;
+}
