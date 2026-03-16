d0c0b23545bba23f75759da1faba1dcf07be8934
AjayBadhe <ajaybadhe2024@gmail.com>
2026-03-16 21:49:51 +0530

    Passing_Ball

---
+++ passing_ball.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+      int n;
+      cin>>n;
+      string s;
+      cin>>s;
+      int cnt=0;
+      for(int i=0;i<s.size();i++){
+           if(s[i]=='R'){
+            cnt++;
+         }
+        else{
+         cnt++;
+         break;
+        }
+      }
+      cout<<cnt<<endl;
+    }
+return 0;
+}
